ace031d259784a7f6cacbf09bb248a55c9b8caa6
aaravm <32593731+aaravm@users.noreply.github.com>
2023-07-01 11:41:06 +0530

    Add files via upload

--- scripts/abt.js
+++ scripts/abt.js
@@ -9,7 +9,7 @@ form1.addEventListener('click', e=>{
 })
 
 // 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
-const accessToken = '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';
+const accessToken = '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';
 const apiUrl = 'https://api.github.com';
 const container = document.getElementById('project-container');
 fetch(`${apiUrl}/user/repos`, {
@@ -56,7 +56,7 @@ fetch(`${apiUrl}/user/repos`, {
 
 
 
-  const accessToken2 = 'AQWhQBlh6iAxe4LGYs5E0Q9ekPReuOx_OhHERQHCic3fUDbfakD_KdaMrZn82PINTDAnyrluN2wmZe-3KHv8paTEvLdmZkZuOkG1lEjtURXA1OYtAU9WIK7l_Mmi5lRv8p9EiKFmh0EyRcDd8gsrR1MAMxqqU56sikkEo-SHAjBguQzXwO7EXOmdlQ5oeOUnRj9uUCOvy-_xQNPJnyapwBPgW2RNhj2s1fbs-JwQl2Oe5B_uJlhK1dFIqfBKlibjAq4G_v38Rk6FkKT9e3dvMz5RteC8Z0P2GzJjjTwRsBauus5jr3a6b8wUyH6GHEB4Kwpb1jMXvRAhzQtA8dibvCYjxv_KFw';
+  const accessToken2 = 'AQUnquk45qX2gaY1vXtBS6BOQvHs1B_uKQSDkq9FPizwh6wPz9npck13dV3J-Oerbt9S-pFZghkzFaG0hwbtIaRbYXwe0_RTyafeljcK1XdkDC5gJZiH3HeACYDX7bARUspRTfQPJZAWv0Tk-b2rEAarOUOn60OV2x5R8VRRZPq_FpG0SC_TyqyZIBhv2LuFraDLRyNkv8MIcC8FW-zqb67FN9qJ9fNgGbtugz919Gn8efX2O0g-EQsbZAB-eIXFM32EbKYDRnuh-GaW6VGwhPvExDg1fGX1PvR7HgIoZa7OyeANEHw1BbgvrJY9iT4EJPIi_QCDz0nuc51Bql00BXBsGtyhSQ';
   const apiUrl2 = 'https://api.linkedin.com/v2';
 
 fetch(`${apiUrl2}/me`, {
