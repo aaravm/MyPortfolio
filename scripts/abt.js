80d66df13813ccbb13c012e0360c092b96545f1f
aaravm <32593731+aaravm@users.noreply.github.com>
2023-07-01 11:46:09 +0530

    Add files via upload

--- scripts/abt.js
+++ scripts/abt.js
@@ -21,8 +21,10 @@ fetch(`${apiUrl}/user/repos`, {
   .then(data => {
     // Process the repository data
     let i=0;
+    console.log("reached before")
     // Display the repositories on your webpage
     data.forEach(project => {
+      console.log(i)
       const projectName = project.name;
       const projectDescription = project.description;
       const projectUrl = project.html_url;
@@ -56,28 +58,32 @@ fetch(`${apiUrl}/user/repos`, {
 
 
 
-  const accessToken2 = 'AQUnquk45qX2gaY1vXtBS6BOQvHs1B_uKQSDkq9FPizwh6wPz9npck13dV3J-Oerbt9S-pFZghkzFaG0hwbtIaRbYXwe0_RTyafeljcK1XdkDC5gJZiH3HeACYDX7bARUspRTfQPJZAWv0Tk-b2rEAarOUOn60OV2x5R8VRRZPq_FpG0SC_TyqyZIBhv2LuFraDLRyNkv8MIcC8FW-zqb67FN9qJ9fNgGbtugz919Gn8efX2O0g-EQsbZAB-eIXFM32EbKYDRnuh-GaW6VGwhPvExDg1fGX1PvR7HgIoZa7OyeANEHw1BbgvrJY9iT4EJPIi_QCDz0nuc51Bql00BXBsGtyhSQ';
-  const apiUrl2 = 'https://api.linkedin.com/v2';
+//   const accessToken2 = 'AQUnquk45qX2gaY1vXtBS6BOQvHs1B_uKQSDkq9FPizwh6wPz9npck13dV3J-Oerbt9S-pFZghkzFaG0hwbtIaRbYXwe0_RTyafeljcK1XdkDC5gJZiH3HeACYDX7bARUspRTfQPJZAWv0Tk-b2rEAarOUOn60OV2x5R8VRRZPq_FpG0SC_TyqyZIBhv2LuFraDLRyNkv8MIcC8FW-zqb67FN9qJ9fNgGbtugz919Gn8efX2O0g-EQsbZAB-eIXFM32EbKYDRnuh-GaW6VGwhPvExDg1fGX1PvR7HgIoZa7OyeANEHw1BbgvrJY9iT4EJPIi_QCDz0nuc51Bql00BXBsGtyhSQ';
+//   const apiUrl2 = 'https://api.linkedin.com/v2';
 
-fetch(`${apiUrl2}/me`, {
-  mode:'no-cors',
-  headers: {
-    'Authorization': `Bearer ${accessToken2}`,
-    'Connection': 'Keep-Alive',
-    'x-li-format': 'json',
+// fetch(`${apiUrl2}/me`, {
+//   mode:'no-cors',
+//   headers: {
+//     'Authorization': `Bearer ${accessToken2}`,
+//     'Connection': 'Keep-Alive',
+//     'x-li-format': 'json',
     
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    // Process the profile data
-    console.log(data);
-    // Display the profile information on your webpage
-  })
-  .catch(error => {
-    // Handle any errors
-    console.error(error);
-  });
+//   }
+// })
+//   .then(response => response.json())
+//   .then(data => {
+//     // Process the profile data
+//     console.log(data);
+//     // Display the profile information on your webpage
+//   })
+//   .catch(error => {
+//     // Handle any errors
+//     console.error(error);
+//   });
+
+
+
+
 
 // // access code: AQBV8unlSghC4MlHL_pKdSjBVdAMZS0fZDAMAO1BtX5PUtwxk7yW3pkK9NMsJZgnNV5mVwaNSdNGvM28xfKh66Jx3Mhcc6CUrekAMjpoRKAwURcyQF2qpTxTX6yE8OFJYYeLe6BZK698hRXCQ72-XkZ5Nopc6iQFrp1NVxhxXu-M7YIgqkIIXJBJGyI7l5FVfZbR0q2a3E6vyg
 
